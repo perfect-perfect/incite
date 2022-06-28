83bc3a830f9782c9b24d9879619dae740b75e766
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-06-28 13:49:01 -0400

    styled almost entire site, will be adding new pages next

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -211,6 +211,25 @@ router.put('/:id', withAuth, (req, res) => {
 });
 
 // DELETE a post /api/posts/:id
+// router.delete('/:id', withAuth, (req, res) => {
+//     Post.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     })
+//         .then(dbPostData => {
+//             if (!dbPostData) {
+//                 res.status(404).json({ message: 'No post found with this id' });
+//                 return;
+//             }
+//             res.json(dbPostData);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
+
 router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
