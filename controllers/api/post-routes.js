57d64af55925f13f914f5675a837a85b1315000b
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-06-29 12:14:13 -0400

    changed cloudinary configuration to utilize cloudinary_url in order to push to heroku

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -15,9 +15,10 @@ const path = require('path');
 const multer = require('multer');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    // cloud_name: process.env.CLOUD_NAME, 
+    // api_key: process.env.CLOUDINARY_API_KEY,
+    // api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloudinary_url: process.env.CLOUDINARY_URL
 })
 
 const storage = new CloudinaryStorage({
